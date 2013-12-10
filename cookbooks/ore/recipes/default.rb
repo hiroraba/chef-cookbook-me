35613aec51f4d916e9541cf09d63554689952979
hiroraba <hiroraba@gmail.com>
2013-12-11 02:01:37 +0900

    remove update

--- cookbooks/ore/recipes/default.rb
+++ cookbooks/ore/recipes/default.rb
@@ -7,16 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
-
 %w{zsh git vim}.each do |pkg|
   package pkg do
     action :install
   end
 end
-
-#file "/etc/sudoers.d/root_ssh_agent" do
-#  mode 0440
-#  owner 'root'
-#  group 'root'
-#  content "Defaults\tenv_keep += \"SSH_AUTH_SOCK\""
-#end
